096f928f3e99ad80d38d1c2b7d4a53db8d384c98
Youhoseong <h0song@naver.com>
2021-11-05 03:01:41 +0900

    test: File Upload api test

--- src/BookStore/BookRegisterComponent/BookmarkTocCheckingView.js
+++ src/BookStore/BookRegisterComponent/BookmarkTocCheckingView.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
 function BookmarkTocCheckingView({navigation, route}) {
     const {fileObj} = route.params;
@@ -7,6 +7,30 @@ function BookmarkTocCheckingView({navigation, route}) {
     return (
         <View>
             <Text>{bookRegisterObj.bookFile.name}</Text>
+
+            <Button title="test" onPress={()=> 
+            {
+                const formData = new FormData();
+                formData.append('files', {
+                    name: bookRegisterObj.bookFile.name,
+                    type: bookRegisterObj.bookFile.type,
+                    uri: bookRegisterObj.bookFile.uri
+
+                });
+
+                fetch('http://localhost:8080/book', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    },
+                    body: formData
+
+                }).then(
+                    (res)=> console.log(res)
+                )
+            }}>
+
+            </Button>
         </View>
     );
 
